af946081c3e4bc14b1c4c2d022dcf9a0d6dbd9f5
David Esner <david@keboola.com>
2021-08-20 14:48:28 +0200

    FIX tests

--- tests/test_component.py
+++ tests/test_component.py
@@ -5,6 +5,7 @@ Created on 12. 11. 2018
 '''
 import os
 import unittest
+from pathlib import Path
 
 import mock
 from freezegun import freeze_time
@@ -14,6 +15,10 @@ from component import Component
 
 class TestComponent(unittest.TestCase):
 
+    def setUp(self) -> None:
+        data_folder = Path(__file__).parent.joinpath('sample-config').as_posix()
+        os.environ['KBC_DATADIR'] = data_folder
+        self.component = Component()
     # set global time to 2010-10-10 - affects functions like datetime.now()
     @freeze_time("2010-10-10")
     # set KBC_DATADIR env to non-existing dir
@@ -24,12 +29,11 @@ class TestComponent(unittest.TestCase):
             comp.run()
 
     def test_add_date_to_url(self):
-        comp = Component()
         url = 'https://www.eshop.cz/expourlorders.csv?patternId=144&dateFrom=2018-1-1&dateUntil=2018-12-31&hash=1234'
 
         expected = 'https://www.eshop.cz/expourlorders.csv?patternId=144&dateFrom=2020-01-01&dateUntil=2021-01-01' \
                    '&hash=1234'
-        changed = comp._add_date_url_parameters(url, '2020-01-01', '2021-01-01')
+        changed = self.component._add_date_url_parameters(url, '2020-01-01', '2021-01-01')
         self.assertEqual(changed, expected)
 
 
